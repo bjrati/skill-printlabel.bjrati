ec3ee3e437502d869ff4ac4d3571c0a8815258af
Brian Resnick <bjrati@gmail.com>
2024-04-05 13:32:49 -0400

    Include the skill logic and the voc files.

--- __init__.py
+++ __init__.py
@@ -25,14 +25,64 @@
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from neon_utils.skills.neon_skill import NeonSkill
+from adapt.intent import IntentBuilder
+from ovos_utils import classproperty
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.decorators import intent_handler
 from .ql800Printer import Printer
 from datetime import datetime
 
 
-class PrintLabel(NeonSkill):  # TODO: Update this class name
-    def __init__(self):
-        super(NewSkill, self).__init__(name="skill-printlabel.bjrati")  # TODO: Update passed name (usually matches class name)
+class PrintLabel(NeonSkill):  
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(network_before_load=False,
+                                   internet_before_load=False,
+                                   gui_before_load=False,
+                                   requires_internet=False,
+                                   requires_network=False,
+                                   requires_gui=False,
+                                   no_internet_fallback=True,
+                                   no_network_fallback=True,
+                                   no_gui_fallback=True)
+                                   
+    @intent_handler(IntentBuilder('PrintLabelIntent').require('PrintKeyword').require('LabelKeyword'))
+    def handle_print_label_intent(self, message):
+        date = datetime.today().strftime('%m/%d/%Y')
+        self.log.info(date)
+
+        # testing extract_number
+        utt = message.data.get('utterance')
+        self.log.info("!BR - " + utt)  # Diagnostic
+        offset_label = utt.find("label")
+        possible_qty_text = utt[:offset_label]
+        self.log.info("Possible qty text: " + possible_qty_text)
+        qty = int(extract_number(possible_qty_text))
+        if qty == 0:
+          # Check for 'to' instead of 'two' and 'for' instead of 'four'
+          if possible_qty_text.find("to") >= 0:
+            qty = 2
+          elif possible_qty_text.find("for") >= 0:
+            qty = 4
+          else:
+            qty = 1
+          pass
+        self.log.info(qty)
+        # Check for description which begins with "for" after "label(s)"
+        post_label_text = utt[offset_label + 5:]
+        offsetFor = post_label_text.find("for")
+        if (offsetFor > -1):
+           description = post_label_text[offsetFor+4:]
+           description = description.upper()
+           self.log.info(description)  # Diagnostic
+           self.printer.print_label_two_lines(date, description, qty)
+        else:
+           self.printer.print_label_one_line(date, qty)
+           pass
+
+    def stop(self):
+        pass           
 
 
-def create_skill():
-    return PrintLabel()
+# def create_skill():
+#    return PrintLabel()
